2f5778314758c36cf6a58319602afc338f18e8d1
Nilesh Payghan <51196316+NileshTech@users.noreply.github.com>
2021-05-12 11:48:16 +0530

    first commit
    Commit to main

---
+++ Assets/Standard Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 6f;
+    public float height = 3f;
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(target == null) return;
+
+        transform.position = target.position;
+
+        transform.position = transform.position + Vector3.forward * distance;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
+
+        transform.LookAt(target);
+    }
+}
+
